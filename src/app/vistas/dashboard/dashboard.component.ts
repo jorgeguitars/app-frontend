0c358b4457ccd8008dfa108a10b9caf29b8eaaf7
jorgemora <jorgeguitars@hotmail.com>
2024-10-19 17:30:11 -0500

    Funcion de posts
    Se crea la funcion de consultar los posts y mostrarlos desde el api de posts

--- src/app/vistas/dashboard/dashboard.component.ts
+++ src/app/vistas/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../servicios/api/api.service';
+import { Router } from '@angular/router';
+import { PostI } from '../../models/listapost.interface';
 
 
 @Component({
@@ -9,13 +11,18 @@ import { ApiService } from '../../servicios/api/api.service';
 })
 export class DashboardComponent implements OnInit{
 
-  constructor(private api:ApiService){}
+  posts: PostI[] = []; // Inicializa posts como un arreglo vacío
 
-ngOnInit(): void {
-  this.api.getAllPosts().subscribe(data =>{
-    console.log(data);
-  })
+  constructor(private api:ApiService, private router:Router){}
+
+  ngOnInit(): void {
+    this.api.getAllPosts().subscribe(data => {
+      console.log(data); // Imprime los datos en la consola para verificar
+      this.posts = data; // Asigna los datos a la variable posts
+    }, error => {
+      console.error('Error al obtener publicaciones', error); // Manejo de errores
+    });
+  }
 
 }
 
-}
